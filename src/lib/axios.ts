20cd97347cdb13b43aa3304572a37dabaaa6e217
Trent Schneweis <10525549+trentschnee@users.noreply.github.com>
2025-02-12 14:38:33 -0600

    Update environment variables and axios base URL configuration

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import Axios from "axios";
 import { env } from "process";
 
 export const api = Axios.create({
-  baseURL: env.API_URL,
+  baseURL: env.FETCH_FETAKEHOME_BASE_API_UR,
 });
 
 // todo: auth request interceptor
