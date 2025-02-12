2f9da14badc36a7648e58424b3d0b4b4b1bd4ee0
Trent Schneweis <10525549+trentschnee@users.noreply.github.com>
2025-02-12 15:45:49 -0600

    Implement unauthorized handling and enhance axios configuration

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,14 +1,28 @@
 import { QueryClient } from '@tanstack/react-query';
+import { isAxiosError } from 'axios';
+/**
+ * Remove the token from the local storage and redirect to the login page
+ */
+const createRetryHandler = (handleUnauthorized: () => void, maxRetries = 2) => {
+  return (count: number, error: Error) => {
+    if (isAxiosError(error) && error.response?.status === 401) {
+      handleUnauthorized();
+      return false
+    }
+    return count < maxRetries;
+  }
 
-export const queryClient = new QueryClient({
+
+}
+export const createQueryClient = (handleUnauthorized: () => void) => new QueryClient({
   defaultOptions: {
     queries: {
-      retry: 2, // Retry failed requests twice
+      retry: createRetryHandler(handleUnauthorized), // Retry failed requests twice
       staleTime: 1000 * 60 * 5, // Data is fresh for 5 minutes
       refetchOnWindowFocus: false, // Disable refetch on window focus
     },
     mutations: {
-      retry: 1, // Retry failed mutations once
+      retry: createRetryHandler(handleUnauthorized, 1), // Retry failed mutations once
     },
   },
 });
