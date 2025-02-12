14354881213b39c4bf5fcae41a947df6c9705ca0
Trent Schneweis <10525549+trentschnee@users.noreply.github.com>
2025-02-12 00:25:19 -0600

    add Axios and React Query setup, including API configuration and provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,13 @@
+import { ReactQueryProvider } from '@/app/providers/react-query-provider'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <ReactQueryProvider>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,7 +20,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </ReactQueryProvider>
   )
 }
 
