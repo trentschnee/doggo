3a6dff934b7bd91eb9e6ae38745d1c2e4db60ca6
Trent Schneweis <10525549+trentschnee@users.noreply.github.com>
2025-02-26 11:34:37 -0600

    feat: enhance DogCard component with favorite toggle and additional dog details

--- src/features/dogs/components/organisms/dog-card.tsx
+++ src/features/dogs/components/organisms/dog-card.tsx
@@ -1,25 +1,35 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Dog } from "@/features/dogs/types/models/dog";
+import { Heart } from "lucide-react";
 import { FC } from "react";
+import { Button } from "../../../../components/ui/button";
 interface DogCardProps {
   dog: Dog
+  isFavorite: boolean
+  onFavoriteToggle: (dog: Dog) => void
 }
-export const DogCard: FC<DogCardProps> = ({ dog }) => (<Card >
+export const DogCard: FC<DogCardProps> = ({ dog, onFavoriteToggle, isFavorite }) => (<Card >
   <CardHeader>
     <img
       alt={`Photo of ${dog.name}`}
-      className="aspect-square h-48 w-full object-cover"
+      className="aspect-square w-full object-cover rounded-md"
       src={dog.img}
     />
-    <CardTitle>{dog.name}</CardTitle>
-    <CardDescription>
-      <ul>
-        <li>Breed: {dog.breed}</li>
-        <li>Age: {dog.age}</li>
-      </ul>
-    </CardDescription>
+    <CardTitle className="font-medium text-lg">{dog.name}</CardTitle>
+
+
   </CardHeader>
   <CardContent>
 
+    <div className="flex flex-col">
+      <div><span className="font-medium">Breed:</span> {dog.breed}</div>
+      <div><span className="font-medium">Age:</span> {dog.age} {dog.age === 1 ? "year" : "years"}</div>
+      <div><span className="font-medium">Location (zip):</span> {dog.zip_code}</div>
+    </div>
   </CardContent>
+
+  <CardFooter>
+    <Button variant="ghost" size="icon" onClick={() => onFavoriteToggle(dog)} className={isFavorite ? 'text-red-300' : 'text-grey-300'} aria-label={isFavorite ? "Remove from favorites" : "Add to favorites"}
+    >  <Heart className={`h-5 w-5 ${isFavorite ? "fill-current" : ""}`} /></Button>
+  </CardFooter>
 </Card>)
