4472d22c61f73d6e716bfdbfec8ab9ef927ec97c
Trent Schneweis <10525549+trentschnee@users.noreply.github.com>
2025-02-27 09:56:24 -0600

    feat: add Vercel Analytics integration to track application performance

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,20 @@
 import { ReactQueryProvider } from '@/providers/react-query-provider'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import { Analytics } from "@vercel/analytics/react"
+
 import { routes } from './routes'
 const router = createBrowserRouter(routes)
 
 function App() {
 
-  return (
+  return (<>
+    <Analytics />
+
     <ReactQueryProvider>
       <RouterProvider router={router} />
     </ReactQueryProvider>
+  </>
   )
 }
 
